4d1164de2975e22daec705f9c5cc094c8460fd26
JTMonitoring <86123318+JTMonitoring@users.noreply.github.com>
2021-06-20 10:25:43 -0700

    Update chores.py

--- chores.py
+++ chores.py
@@ -1,9 +1,29 @@
 import os
 import sys
+#text to speech
+from gtts import gTTS
+
+language = 'en'
+
+
+
+def mytts(mytext, language):
+    # language = 'en'
+    ttsobj = gTTS(text=mytext, lang=language, slow=False)
+    ttsobj.save("respond.mp3")
+    os.system("mpg321 respond.mp3")
+    os.system("rm respond.mp3")
+
+
+
+
+#responces
+not_understood = "Sorry, I don't recognize that."
+
 
 #keywords from transcript (triggers)
-start_task_triggers = ["firefox", "update", "upgrade", "shutdown", "reboot", "security"]
-start_task_commands = ["firefox", "sudo apt update", "sudo apt upgrade -y", "shutdown", "reboot", "python3 ping2.py", "nothing"]
+start_task_triggers = ["firefox", "update", "upgrade", "shutdown", "reboot", "security", "scan"]
+start_task_commands = ["firefox", "sudo apt update", "sudo apt upgrade -y", "shutdown", "reboot", "python3 ping2.py", "sudo python3 netscan.py -t 192.168.0.1/24"]
 
 #security triggers/commands:
 security_task_triggers = ["start security", "disable security", "read log"]
@@ -28,12 +48,16 @@ def think(transcript):
                 
                 if word in start_task_triggers:
                     index = start_task_triggers.index(word)
-                
-            print(start_task_commands[index])
-            os.system(start_task_commands[index])
-                
+                    print(start_task_commands[index])
+                    os.system(start_task_commands[index])
+                    break
+                else:
+                    not_in += 1
+                    continue
         except:
             print("sorry, I dont understand..")
+            mytts(not_understood, "en")
+            break
     
         
 
